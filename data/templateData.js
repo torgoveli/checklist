a1c308d8102e7743b7ca1f73c42b576612fa1510
JenyaKalinin <torgoveli123@gmail.com>
2019-03-25 03:03:38 +0300

    search error

---
+++ data/templateData.js
@@ -0,0 +1,10 @@
+export const templateData = [
+    { name: 'Devin', desc: 'Desc test esc test esc test' },
+    { name: 'Jackson', desc: 'Desc test esc test esc test' },
+    { name: 'James', desc: 'Desc test esc test esc test' },
+    { name: 'Joel', desc: 'Desc test esc test esc test' },
+    { name: 'John', desc: 'Desc test esc test esc test' },
+    { name: 'Jillian', desc: 'Desc test esc test esc test' },
+    { name: 'Jimmy', desc: 'Desc test esc test esc test' },
+    { name: 'Julie', desc: 'Desc test esc test esc test' },
+]
